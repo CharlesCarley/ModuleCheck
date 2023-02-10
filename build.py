bb12e9140c2a62379726807a38d079af5d505d7b
Charlie C <snailrose@gmail.com>
2023-02-10 15:01:00 -0700

    Update

--- build.py
+++ build.py
@@ -1,38 +1,91 @@
 import os, github
+import Utils
+
+
 
 class GitHubAccount:
     def __init__(self, rootDir):
-        self.account = "CharlesCarley"
-        self.github = ""
-        self.home = rootDir
+        self._account = "CharlesCarley"
+        self._home = Utils.Path(os.path.abspath(rootDir))
+        self._cred = self.home().subdir(".tokens")
 
-    def getPublicRepoList(self):
-        os.chdir(self.home)
-        gh = github.Github()
+    def home(self):
+        return self._home
+    def credentials(self):
+        return self._cred
 
+    def _token(self, name):
+        token = None
+        try:
+            fp = self.credentials().open(name)
+            token = fp.read()
+            fp.close()
+        except IOError:
+            print("Failed to read access token")
+        return token
+
+    def _repos(self):
+        lines = []
+        try:
+            fp = self.credentials().open("repos.txt")
+            rl = fp.readlines()
+            for line in rl:
+                lines.append(line.replace("\r", "").replace("\n", ""))
+            fp.close()
+        except IOError:
+            print("Failed to read repo list")
+
+        return lines
+
+    def _public(self):
+        return self._token("pub.txt")
+
+    def repos(self):
+        gh = github.Github(self._public())
         repos = gh.get_user().get_repos()
+
+        wanted = self._repos()
 
         repoList = {}
         for repo in repos:
             if (repo.fork): continue
-            if (repo.owner.login != self.account): continue
-            if (repo.private): continue
+            if (repo.owner.login != self._account): continue
+            if repo.name in wanted:
+                print("".ljust(2, ' '), "repository:", repo.name)
+                repoList[repo.name] = repo
+        return repoList
 
-            repoList[repo.name] = repo.clone_url
+    def clone(self):
+        self.home().goto()
+        print("Cloning into", self.home())
 
-        return repoList;
+        cloneList = self.repos()
+        for repo in cloneList.values():
+            if (not os.path.isdir(repo.name)):
+                self.home().run("git clone %s" %repo.ssh_url)
+            else:
+                print("Skipping", repo.name, ":", repo.ssh_url)
+                print("".ljust(2, ' '), "directory exists")
 
-    
+        self.home().goto()
+
+    def removeRepos(self):
+        self.home().goto()
+
+        cloneList = self.repos()
+        for repo in cloneList.values():
+            print("removing repo.name ", repo.name)
+            self.home().subdir(repo.name).remove()
+
+        self.home().goto()
+ 
+
 
 def main():
     mgr = GitHubAccount(os.path.abspath(os.getcwd()))
-    mgr.getPublicRepoList()
+    mgr.clone()
+    mgr.removeRepos()
 
 
-
-
-
-    
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
